6eef625ef9a9163fa0227e7e346d1ac5d79a0de5
Girish Kumar <163101858+imgirish07@users.noreply.github.com>
2024-06-26 14:07:10 +0530

    Delete server/routes/expense.js

--- server/routes/expense.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const {handleExpense, handleRemoveExpense} = require('../controller/Expense');
-const router = express.Router();
-
-router.post('/addexpense', handleExpense);
-router.post('/remove-expense',handleRemoveExpense);
-
-module.exports = router;
